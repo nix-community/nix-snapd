4ee7ccd7d652fa4b58fad881003b5bc54af9e9d6
Benjamin Levy <7348004+io12@users.noreply.github.com>
2023-11-25 01:13:50 -0500

    Precreate it before apparmor loads

--- src/nixos-module.nix
+++ src/nixos-module.nix
@@ -57,6 +57,15 @@ in
       packages = [ snap ];
       sockets.snapd.wantedBy = [ "sockets.target" ];
       services.snapd.wantedBy = [ "multi-user.target" ];
+      services.nix-snapd-precreate = {
+        wantedBy = [ "apparmor.service" ];
+        before = [ "apparmor.service" ];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart =
+            "${pkgs.coreutils}/bin/mkdir -p /var/lib/snapd/apparmor/snap-confine";
+        };
+      };
     };
 
     security.wrappers.snap-confine-stage-1 = {
